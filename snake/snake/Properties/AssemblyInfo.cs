e18c94c5ec5d66ac0fe8af76f2a39b8935963c57
chinchopa <chinchopa23@mail.ru>
2018-09-19 09:49:40 +0500

    123

--- snake/snake/Properties/AssemblyInfo.cs
+++ snake/snake/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("snake")]
+[assembly: AssemblyTitle("ConsoleApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("snake")]
+[assembly: AssemblyProduct("ConsoleApplication1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("50cd4a76-1a41-43b4-bfdf-0c0502a5536e")]
+[assembly: Guid("d73ce2b1-7860-4521-91cf-bd9c4f1e2053")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
